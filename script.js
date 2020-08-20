6be1e1781773c137ee55fd14869471802c9ce4af
Nicole Frimpong <nicolefri123@gmail.com>
2020-08-20 14:31:15 +0000

    Everything

--- script.js
+++ script.js
@@ -4,55 +4,62 @@ function yourName(){
   let name = prompt("Enter Your Name");
   console.log(name);
   var pTag = document.getElementById("name");
-  pTag.innerHTML = ("Hi " + name + ":) I hope your day has been going well! Welcome again, to my blog.")
-}
-      
+  pTag.innerHTML = ("Hi " + name + ":) I hope your day has been going well! Welcome again, to my blog.")}
 function yourAge(){
   console.log("Function started");
   let age = prompt("Enter Your Age");
   console.log(age);
   var pTag = document.getElementById("age");
-  pTag.innerHTML = ("Wow only " + age + " years young! Live life to the fullest.")
-}
-
+  pTag.innerHTML = ("Wow only " + age + " years young! Live life to the fullest.")}
 function favoriteSport(){
   console.log("Function started");
   let sport = prompt("Enter Favorte Sport");
   console.log(sport);
   var pTag = document.getElementById("sport");
-  pTag.innerHTML = ("I love " + sport + " too! You must be very athletic or quick minded to love " + sport + " !")
-}
-
+  pTag.innerHTML = ("I love " + sport + " too! You must be very athletic or quick minded to love " + sport + " !")}
 function favoriteFood(){
   console.log("Function started");
   let food = prompt("Enter Favorte Food");
   console.log(food);
   var pTag = document.getElementById("food");
-  pTag.innerHTML = ("I am hungry now! " + food + " is a great go to meal.")
-}
+  pTag.innerHTML = ("I am hungry now! " + food + " is a great go to meal.")}
+
 
 var amountOfLikes = 0
-//Like Button Code
-function handleLike(){
+function handleLikes(){
   amountOfLikes++
   console.log(amountOfLikes)
   var likesDisplay = document.getElementById("likes")
-  likesDisplay.innerHTML = amountOfLikes
-}
-
-var amountOImpartial = 0
+  likesDisplay.innerHTML = amountOfLikes}
+var amountOfImpartial = 0
 function handleImpartial(){
   amountOfImpartial++
   console.log(amountOfImpartial)
   var impartialDisplay = document.getElementById("impartial")
-  impartialDisplay.innerHTML = amountOfImpartial
-}
-
+  impartialDisplay.innerHTML = amountOfImpartial}
 var amountOfDislikes = 0
-//Like Button Code
-function handleDisikes(){
+function handleDislikes(){
   amountOfDislikes++
   console.log(amountOfDislikes)
   var dislikesDisplay = document.getElementById("dislikes")
-  dislikesDisplay.innerHTML = amountOfDislikes
-}
+  dislikesDisplay.innerHTML = amountOfDislikes}
+
+function likeGif(){
+console.log("Function started");
+  let sport = prompt("Enter Favorte Sport");
+  console.log(sport);
+  var pTag = document.getElementById("sport");
+  pTag.innerHTML = ("I love " + sport + " too! You must be very athletic or quick minded to love " + sport + " !")}
+
+var $ = jQuery;
+$('.thumbnail img').click(function(event) {
+  $('.hero img').replaceWith($(this).clone());
+});
+
+
+function theButton(){
+  console.log("Function started");
+  let theButton = prompt("Thank You! Your information has been received. Enter initials and press ok to confirm.");
+  console.log("Thank you! Your information has been received. Enter initials and press ok to confirm.");
+  var pTag = document.getElementById("theButton");
+  pTag.innerHTML = (".")}
